ffd1f69b4254c3b04419f048cb078fed1f5d8b9c
Rodrigo Morais <rcastromorais01@gmail.com>
2023-08-28 18:47:19 -0300

    first commit

---
+++ src/main/java/com/portal/portalapi/PortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.portal.portalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portal/portalapi/PortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.portalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
