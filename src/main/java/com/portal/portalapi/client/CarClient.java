7ce688e472185ce9b37f94ef5582f32c1293019c
Rodrigo Morais <rcastromorais01@gmail.com>
2023-08-31 21:51:13 -0300

    webclient basic config

--- src/main/java/com/portal/portalapi/client/CarClient.java
+++ src/main/java/com/portal/portalapi/client/CarClient.java
@@ -2,16 +2,27 @@ package com.portal.portalapi.client;
 
 import com.portal.portalapi.db.Car;
 import com.portal.portalapi.db.Owner;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class CarClient {
@@ -43,9 +54,30 @@ public class CarClient {
         rest.delete(SALES_CLIENT_URI+"/car"+id);
     }
 
-    /* Reminder ---- try to use webclient
+    public void test() {
+        WebClient client = WebClient.builder()
+                .baseUrl("http://localhost:8080")
+                .defaultCookie("cookieKey", "cookieValue")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultUriVariables(Collections.singletonMap("url", "http://localhost:8080"))
+                .build();
 
-     */
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS))
+                .addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS)));
+
+        WebClient novo = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
+
+
+    }
+
+
 
 
 }
